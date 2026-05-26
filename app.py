129ecfa3869d7dbb7ed92be74fe7892938b7eca1
Lutain-Brogy <brogylutain@gmail.com>
2026-05-26 04:42:46 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("Firestore Rule Analyzer")
+st.title("Your database is exposed checker")
 
 mode = "normal"  # later you can change this to selectbox
 
@@ -11,8 +11,8 @@ user_line = st.text_input("Type the first Firestore rule line")
 if user_line:
 
     if user_line == correct_first_line:
-        st.success("Correct first line.")
-        st.code(user_line)
+        st.write('Correct first line') 
+        st.code(f'user_line')
 
     else:
         st.error("The first line is wrong.")
@@ -29,4 +29,31 @@ if user_line:
                 )
 
         if len(user_line) != len(correct_first_line):
+            st.warning("Length mismatch between input and expected rule.")
+
+correct_second_line = "match /database/{b} {"
+
+user_line = st.text_input("Type second line")
+
+if user_line:
+
+    if user_line == correct_second_line:
+        st.write("Correct second line")
+        st.code(user_line)
+
+    else:
+        st.write("The second line is wrong")
+
+        min_len = min(len(user_line), len(correct_second_line))
+
+        for i in range(min_len):
+
+            if user_line[i] != correct_second_line[i]:
+
+                st.write(
+                    f"Error at position {i+1}: "
+                    f"Expected '{correct_second_line[i]}' but got '{user_line[i]}'"
+                )
+
+        if len(user_line) != len(correct_second_line):
             st.warning("Length mismatch between input and expected rule.")
