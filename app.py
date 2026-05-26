eb9da85c205fa98ff647d701aa4e88ee0733218f
Lutain-Brogy <brogylutain@gmail.com>
2026-05-26 03:30:41 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
 import streamlit as st
-st.title("Firestore rule Analyzer")
+
+st.title("Firestore Rule Analyzer")
 
 user_line = st.text_input("Type the first Firestore rule line")
 
 correct_first_line = "service cloud.firestore {"
 
-user_line = "service cloud.firestore {"
+if user_line:  # only run after user types something
 
-if user_line == correct_first_line:
-    st.write("Correct first line.")
-    st.code(f"{user_line}")
-else:
-    st.write("The first line is wrong.")
+    if user_line == correct_first_line:
+        st.success("Correct first line.")
+        st.code(user_line)
+    else:
+        st.error("The first line is wrong.")
+        st.code(f"Expected: {correct_first_line}")
+        st.code(f"Got: {user_line}")
