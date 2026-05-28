a48b23a87c3b5655d16ddff35943b7a0c9275429
Lutain-Brogy <brogylutain@gmail.com>
2026-05-28 03:18:42 +0200

    Update app.py

--- app.py
+++ app.py
@@ -58,6 +58,46 @@ service cloud.firestore {{
 }}
 """)
 
+    if edit_choice =='read authenticated access':
+
+        auth_choce = st.selectbox(
+            'Slect your authentication type of rule'
+            [ "only creater can read",
+              "role-based",
+              "custorm based",
+              "time based"]
+
+        )
+
+       if auth_choice == 'only creater can read':
+            st.write('Write  all values then copy rule') 
+
+        user_input = st.text_input("A, the collection")
+        A = user_input
+
+        B_input = st.text_input("B, the document")
+        C_input = st.text_input("C, the subcollection")
+        D_input = st.text_input("D value")
+
+        B = A if B_input == "any" else B_input
+        C = B if C_input == "any" else C_input
+        D = C if D_input == "any" else D_input
+
+        st.code(f"""
+rules_version = '2';
+
+service cloud.firestore {{
+  match /databases/{{database}}/documents {{
+
+    match /{A}/{{{B}}}/{C}/{{{D}}} {{
+
+      allow read: if request.auth != null
+                  && request.auth.token.admin == true;
+
+    }}
+  }}
+}}
+""")
 
 
        
