2061011bb62e89c9ddaf33e16dc3477e85b84fd2
Lutain-Brogy <brogylutain@gmail.com>
2026-05-28 02:55:58 +0200

    Update app.py

--- app.py
+++ app.py
@@ -33,6 +33,35 @@ if choice == "Allow read only":
         ]
     )
 
+if edit_choice = 'read public access' 
+user_input = st.text_input("A value")
+A = user_input
+
+B_input = st.text_input("B value")
+C_input = st.text_input("C value")
+D_input = st.text_input("D value")
+
+B = A if B_input == "any" else B_input
+C = A if C_input == "any" else C_input
+D = A if D_input == "any" else D_input
+
+st.code(f"""
+rules_version = '2';
+
+service cloud.firestore {{
+  match /databases/{{database}}/documents {{
+
+    match /{A}/{B}/{C}/{D} {{
+
+      allow read: if request.auth != null;
+
+    }}
+  }}
+}}
+""")
+
+
+
 
 
        
