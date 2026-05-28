a007e2f0403e7a2b0bcb680a9e9b94ab74c752ef
Lutain-Brogy <brogylutain@gmail.com>
2026-05-28 03:23:49 +0200

    Update app.py

--- app.py
+++ app.py
@@ -58,7 +58,6 @@ service cloud.firestore {{
 }}
 """)
         if edit_choice == 'read authenticated access':
-
            auth_choice = st.selectbox(
         'Select your authentication type of rule',
         ["only creater can read",
