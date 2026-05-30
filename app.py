35cec8f1a75e3be87e521dcca39631bc21ecde15
Lutain-Brogy <brogylutain@gmail.com>
2026-05-30 15:17:51 +0200

    Update app.py

--- app.py
+++ app.py
@@ -36,10 +36,10 @@ if choice == "Allow read only":
        C_input = st.text_input("C, the subcollection")
        D_input = st.text_input("D value")
 
-      A = A_input
-      B = A if B_input == "any" else B_input
-      C = B if C_input == "any" else C_input
-      D = C if D_input == "any" else D_input
+       A = A_input
+       B = A if B_input == "any" else B_input
+       C = B if C_input == "any" else C_input
+       D = C if D_input == "any" else D_input
 
     st.code(f"""
 rules_version = '2';
