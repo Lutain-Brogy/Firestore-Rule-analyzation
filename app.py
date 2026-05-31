6a3b747660de16f36e6ec970b2c8b0684248d41c
Lutain-Brogy <brogylutain@gmail.com>
2026-05-31 03:52:11 +0200

    Update app.py

--- app.py
+++ app.py
@@ -35,9 +35,9 @@ if choice == "Allow read only":
        D = st.text_input("D value")
 
     
-       B = A if B_input == "any" else B_input
-       C = B if C_input == "any" else C_input
-       D = C if D_input == "any" else D_input
+       B = A if B == "any" else B
+       C = B if C == "any" else C
+       D = C if D == "any" else D
 
     st.code(f"""
 rules_version = '2';
