e732624a747880670a1ad638f7d943bf319685b2
Lutain-Brogy <brogylutain@gmail.com>
2026-07-03 03:34:55 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,3 +6,9 @@ choice = st.selectbox(
     "Choose a mode",
     ["Standard operation", "Learning mode"]
 )
+
+if choice == Standard operation:
+   rule1, rule2, rule3, rule4 = st.tabs(["read type", "write type" , "create role", "read and write type"])
+
+with rule1
+st.write('hey')
