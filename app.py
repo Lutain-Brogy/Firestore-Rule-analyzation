3b26c9df23b6a13e82d5691a4dfb8cf11bd1346e
Lutain-Brogy <brogylutain@gmail.com>
2026-07-04 13:36:38 +0200

    Update app.py

--- app.py
+++ app.py
@@ -124,7 +124,7 @@ service cloud.firestore {{
     match /{A}/{B}/{C}/{D} {{
 
       allow read: if request.auth != null
-                  && request.auth.uid == '{{{E_condition}}}';
+                  && request.auth.uid == '{{{E_choice}}}';
 
     }}
 
