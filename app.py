39339f7f0bb6d6ba366a3a2a54343942663ec300
Lutain-Brogy <brogylutain@gmail.com>
2026-07-07 04:51:01 +0200

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ if choice == 'Standard operation':
                     "🌍 Public",
                     "🔐 Authentication",
                     "👥 Ownership & Membership",
-                    "Time-Based Rules",
+                    "Time based rule",
                     "📄 Document-Based Rules",
                     "🔗 Cross-Document Checks",
                     "Open guider"
@@ -113,7 +113,7 @@ service cloud.firestore {{
   }}
 }}
 """)
-                if read == 'Time-Based Rules':
+                if read == 'Time based rule':
                    st.write('hello')
             
 
