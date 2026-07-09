ebc4ae637163e8deab5e2b623efef883fc48ac52
Lutain-Brogy <brogylutain@gmail.com>
2026-07-09 05:32:22 +0200

    Update app.py

--- app.py
+++ app.py
@@ -148,7 +148,7 @@ service cloud.firestore {{
                       D_option = 'if true;'
                                                      
                                                      
-             st.code(f"""
+        st.code(f"""
 rules_version = '2';
 
 service cloud.firestore {{
