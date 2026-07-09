0628a53b3d7e39ca5679b4a163f9d1997eb39281
Lutain-Brogy <brogylutain@gmail.com>
2026-07-09 06:03:34 +0200

    Update app.py

--- app.py
+++ app.py
@@ -149,7 +149,7 @@ service cloud.firestore {{
                   Y = st.text_input('From when')
                   X = st.text_input('To when')
                   D_option = f"""if request.time >= timestamp.date({Y})
-                            && request.time < timestamp.date({X});
+                                 && request.time < timestamp.date({X});
                                  """
                                 
                                                      
@@ -161,6 +161,7 @@ service cloud.firestore {{
 
     match /{A}/{B}/{C} {{
       allow read: {D_option}
+      
     }}
 
   }}
